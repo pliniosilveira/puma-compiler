0afdc1da476139727ad04cc97823cf9b40d5ffa2
Yago Boff Liborio <yago.liborio@acad.pucrs.br>
2020-01-28 12:25:16 -0300

    security and pre compiler

--- include/puma.h
+++ include/puma.h
@@ -18,7 +18,7 @@ struct CompilerOptions {
 
         GraphPartitioningScheme gp_ = GP_ROW_MAJOR;
         bool coalesceMVMOperations_ = true;
-        bool printDebugInfo_ = false;
+        bool printDebugInfo_ = true;
 
 };
 
